4810fc1e01fb7d1920618cfdcad4149cf697d21c
Matheus Silva <matheushenrique2773@gmail.com>
2024-04-20 21:06:02 -0300

    formating projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material'
+import Image from 'next/image'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 export default function Projects({ refScroll }) {
 	return (
@@ -7,10 +11,17 @@ export default function Projects({ refScroll }) {
 			<Typography variant="h4">
 						Projects
 			</Typography>
-			<Box className='flex flex-row gap-x-4'>
-				<img src="https://via.placeholder.com/150" alt="Picture of the author"/>
-				<img src="https://via.placeholder.com/150" alt="Picture of the author"/>
-				<img src="https://via.placeholder.com/150" alt="Picture of the author"/>
+			<Image src="/images/lesc.png" alt="The Brooklyn Brothers" height={150} width={150} className='rounded-lg'/>
+			<Typography variant="h5" className='text-center'>
+				Liga de Empreendedorismo de São Carlos
+			</Typography>
+			<Typography variant="body1" className='text-center'>
+				The biggest project I've worked on. I am the General Director of the project that aims to encourage innovation and entrepreneurship. We currently have some projects underway, such as a podcast "Fala Sanca", a TedTalks "LESCTalks", a startup accelerator "Starting UP" and an internal training project "LESChool".
+			</Typography>
+			<Box className='flex flex-row gap-x-8 justify-center items-center'>
+				<InstagramIcon className='cursor-pointer' onClick={() => window.open('https://www.instagram.com/lesc_sanca')}/>
+				<LinkedInIcon className='cursor-pointer' onClick={() => window.open('https://www.linkedin.com/company/lescsanca')}/>
+				<FacebookIcon className='cursor-pointer' onClick={() => window.open('https://www.facebook.com/ligadeempreendedorismosc?mibextid=ZbWKwL')}/>
 			</Box>
 		</Box>
 	)
