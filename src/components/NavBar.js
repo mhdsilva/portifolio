80f93c3820f76dfa549e0adb5324551dc35e70ea
Matheus Silva <matheushenrique2773@gmail.com>
2024-04-20 21:29:05 -0300

    formating logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppBar, Button, Container, Link, Typography } from '@mui/material'
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 
 export default function NavBar({ scroll }){
 
@@ -12,7 +13,10 @@ export default function NavBar({ scroll }){
 			<Container className='max-w-5xl	mx-auto h-max flex items-center justify-center'>
 				<Container className='cursor-pointer'	>
 					<a onClick={() => handleClick('introduction')}>
-						<Typography> LOGO </Typography>
+						<Typography variant="h6" className='text-white flex items-center'>
+							<ElectricBoltIcon className='text-primary' />
+							MHS
+						</Typography>
 					</a>
 				</Container>
 				<Container className='flex flex-row justify-end items-center gap-x-4'>
